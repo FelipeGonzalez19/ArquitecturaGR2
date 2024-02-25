5b4b63f7711960144a8479dccf8d568d74e4e678
madiazf1 <ma.diazf1@uniandes.edu.co>
2024-02-25 12:38:20 -0500

    Commit 001: se actualiza send.py

--- send.py
+++ send.py
@@ -1,14 +1,27 @@
-import pika
+import pika, os, random
+
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
+messagesToSend = random.randint(150, 300)
+
+print('start to send ', messagesToSend)
+global count
+  # Send
+
+count = 0
+
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='arqsub') # Declare a queue
+    
+for x in range(messagesToSend):
+
+    channel.basic_publish(exchange='',
+                      routing_key='arqsub',
+                      body='ALERT'+str(x))
+
+    count = count + 1
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-channel = connection.channel()
-
-
-channel.queue_declare(queue="hello")
-
-body = "Hello World!"
-
-channel.basic_publish(exchange="", routing_key="hello", body=body)
-print(" [x] Sent 'Hello World!'")
+print(count, ' messages sent ')
 connection.close()
